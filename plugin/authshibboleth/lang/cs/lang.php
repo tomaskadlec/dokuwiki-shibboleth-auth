008269957879e79d0c0ce44c27e80e108629a837
Tomáš Kadlec <kadleto2@fit.cvut.cz>
2015-12-29 20:12:40 +0100

    Login form - translations cs, en

---
+++ plugin/authshibboleth/lang/cs/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Translations - czech
+ *
+ * @author  Ivan Novakov http://novakov.cz/
+ * @license http://debug.cz/license/bsd-3-clause BSD 3 Clause 
+ * @link https://github.com/ivan-novakov/dokuwiki-shibboleth-auth Homepage
+ * 
+ * @link https://www.dokuwiki.org/devel:configuration#default_settings Documentation
+ */
+$lang['login_authshibboleth'] = 'Shibboleth SSO';
+$lang['login_authshibboleth_submit'] = 'Přihlásit se';
---
+++ plugin/authshibboleth/lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Translations - english
+ *
+ * @author  Ivan Novakov http://novakov.cz/
+ * @license http://debug.cz/license/bsd-3-clause BSD 3 Clause 
+ * @link https://github.com/ivan-novakov/dokuwiki-shibboleth-auth Homepage
+ * 
+ * @link https://www.dokuwiki.org/devel:configuration#default_settings Documentation
+ */
+$lang['login_authshibboleth'] = 'Shibboleth SSO';
+$lang['login_authshibboleth_submit'] = 'Login';
